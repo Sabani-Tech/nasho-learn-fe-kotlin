356db774c3d4c533ba82fd6e10e281aec16ce263
chairoel <chairoelamri2610@gmail.com>
2024-11-06 18:56:17 +0700

    update injection

--- app/src/main/java/com/learn/nasho/di/Injection.kt
+++ app/src/main/java/com/learn/nasho/di/Injection.kt
@@ -1,4 +1,23 @@
 package com.learn.nasho.di
 
+import android.content.Context
+import com.learn.nasho.data.locale.datastore.DataStorePreferences
+import com.learn.nasho.data.locale.datastore.DataStorePreferencesImpl
+import com.learn.nasho.data.locale.datastore.dataStore
+import com.learn.nasho.data.remote.api.ApiConfig
+import com.learn.nasho.data.remote.api.ApiService
+import com.learn.nasho.data.repository.UserRepository
+import com.learn.nasho.data.repository.UserRepositoryImpl
+
 object Injection {
+    private fun provideApiService(): ApiService = ApiConfig.getApiService()
+
+    private fun provideDataStorePref(context: Context): DataStorePreferences =
+        DataStorePreferencesImpl(context.dataStore)
+
+    fun provideUserRepository(context: Context): UserRepository {
+        val apiService = provideApiService()
+        val dataStorePreferences = provideDataStorePref(context)
+        return UserRepositoryImpl(apiService, dataStorePreferences)
+    }
 }
