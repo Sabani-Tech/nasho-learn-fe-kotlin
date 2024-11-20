5497929b243e3ce602706020376629f6bbbb4ed4
Amrul <chairoelamri2610@gmail.com>
2024-11-20 07:04:56 +0700

    implement  API get material list by category id and show the data

--- app/src/main/java/com/learn/nasho/ui/views/MaterialListActivity.kt
+++ app/src/main/java/com/learn/nasho/ui/views/MaterialListActivity.kt
@@ -10,11 +10,15 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.learn.nasho.R
 import com.learn.nasho.data.ResultState
 import com.learn.nasho.data.enums.CategoryType
 import com.learn.nasho.data.remote.dto.CategoryDto
+import com.learn.nasho.data.remote.dto.MaterialDto
 import com.learn.nasho.databinding.ActivityMaterialListBinding
+import com.learn.nasho.ui.adapters.MaterialAdapter
+import com.learn.nasho.ui.adapters.RecyclerViewClickListener
 import com.learn.nasho.ui.viewmodels.material.CategoryListViewModel
 import com.learn.nasho.ui.viewmodels.material.MaterialListViewModel
 import com.learn.nasho.ui.viewmodels.material.MaterialViewModelFactory
@@ -24,6 +28,9 @@ import com.learn.nasho.utils.parcelable
 class MaterialListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMaterialListBinding
+
+    private lateinit var materialAdapterPhase1: MaterialAdapter
+    private lateinit var materialAdapterPhase2: MaterialAdapter
 
     private lateinit var factory: MaterialViewModelFactory
     private val materialListViewModel: MaterialListViewModel by viewModels {
@@ -53,70 +60,108 @@ class MaterialListActivity : AppCompatActivity() {
                 }
             }
 
-//            materialListViewModel.materialList.observe(this@MaterialListActivity) { resultState ->
-//                when (resultState) {
-//                    is ResultState.Success -> {
-//                        val response = resultState.data
-//                        if (response.error == true) {
-//                            Toast.makeText(
-//                                this@MaterialListActivity,
-//                                getString(R.string.category_failed, response.message),
-//                                Toast.LENGTH_LONG
-//                            ).show()
-//                        } else {
-
-//                            response.data?.forEach { category ->
-//                                with(binding) {
-//                                    Log.d("TAG", "onCreate: category type: ${category.type}")
-//                                    if (category.type == CategoryType.SHARAF.type.lowercase()) {
-//                                        sharaf = category
-//                                        tvDescSharaf.text = sharaf.desc
-//                                        btnStartLearSharaf.isEnabled = true
-//                                    }
-//
-//                                    if (category.type == CategoryType.NAHWU.type.lowercase()) {
-//                                        nahwu = category
-//                                        tvDescNahwu.text = nahwu.desc
-//                                        btnStartLearNahwu.isEnabled = true
-//                                    }
-//
-//                                }
-//                            }
-//                        }
-//                    }
-//
-//                    is ResultState.Error -> {
-//                        Toast.makeText(
-//                            this@MaterialListActivity,
-//                            getString(R.string.category_failed, resultState.message),
-//                            Toast.LENGTH_LONG
-//                        ).show()
-//                    }
-//
-//                    else -> {}
-//                }
-//
-//            }
-
-            if (data.type == CategoryType.NAHWU.type) {
-                binding.ivIllustration.setImageDrawable(
-                    ContextCompat.getDrawable(this, R.drawable.img_onboard_nahwu)
-                )
-            } else if (data.type == CategoryType.SHARAF.type) {
-                binding.ivIllustration.setImageDrawable(
-                    ContextCompat.getDrawable(this, R.drawable.img_onboard_sharaf)
-                )
-            }
-
             binding.apply {
+
+                if (data.type == CategoryType.NAHWU.type) {
+                    ivIllustration.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this@MaterialListActivity,
+                            R.drawable.img_onboard_nahwu
+                        )
+                    )
+                } else if (data.type == CategoryType.SHARAF.type) {
+                    ivIllustration.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this@MaterialListActivity,
+                            R.drawable.img_onboard_sharaf
+                        )
+                    )
+                }
+
                 "${data.type} (${data.id})".also { tvMaterialTitle.text = it }
+                tvDesc.text = data.desc
+
+                // Setup Phase 1 Adapter
+                materialAdapterPhase1 =
+                    MaterialAdapter(object : RecyclerViewClickListener {
+                        override fun onItemClicked(position: Int) {
+                            // Handle click for Phase 1
+                            handleItemClick(position, phase = 1)
+                        }
+                    })
+                rvMaterial1.setHasFixedSize(true)
+                rvMaterial1.layoutManager = LinearLayoutManager(this@MaterialListActivity)
+                rvMaterial1.adapter = materialAdapterPhase1
+
+
+                // Setup Phase 2 Adapter
+                materialAdapterPhase2 =
+                    MaterialAdapter(object : RecyclerViewClickListener {
+                        override fun onItemClicked(position: Int) {
+                            // Handle click for Phase 2
+                            handleItemClick(position, phase = 2)
+                        }
+                    })
+                rvMaterial2.apply {
+                    setHasFixedSize(true)
+                    layoutManager = LinearLayoutManager(this@MaterialListActivity)
+                    adapter = materialAdapterPhase2
+                }
+
             }
 
+            materialListViewModel.materialList.observe(this@MaterialListActivity) { resultState ->
+                when (resultState) {
+                    is ResultState.Success -> {
+                        val response = resultState.data
+                        if (response.error == true) {
+                            Toast.makeText(
+                                this@MaterialListActivity,
+                                getString(R.string.material_failed, response.message),
+                                Toast.LENGTH_LONG
+                            ).show()
+                        } else {
+                            response.data?.let {
+                                Log.d("MaterialListActivity", "Data: ${it.size}")
+                                materialAdapterPhase1.setItems(it)
+                            }
+                        }
+                    }
+
+                    is ResultState.Error -> {
+                        Toast.makeText(
+                            this@MaterialListActivity,
+                            getString(R.string.material_failed, resultState.message),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                    else -> {}
+                }
+            }
         }
     }
 
     override fun onResume() {
         super.onResume()
-//        categoryId.value?.let { materialListViewModel.getMaterialListByCategory(it) }
+        categoryId.value?.let { materialListViewModel.getMaterialListByCategory(it) }
+    }
+
+    // Handle different list clicks based on the phase
+    private fun handleItemClick(position: Int, phase: Int) {
+        // Do something different based on phase
+        when (phase) {
+            1 -> {
+                // Handle click for Phase 1 item
+                Toast.makeText(this, "Clicked item in Phase 1: $position", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            2 -> {
+                // Handle click for Phase 2 item
+                Toast.makeText(this, "Clicked item in Phase 2: $position", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }
