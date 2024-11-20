2d93c81325c077495f838f4849578b73fab5c674
Amrul <chairoelamri2610@gmail.com>
2024-11-20 07:03:50 +0700

    setup material list adapter

---
+++ app/src/main/java/com/learn/nasho/ui/adapters/MaterialAdapter.kt
@@ -0,0 +1,81 @@
+package com.learn.nasho.ui.adapters
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.learn.nasho.data.remote.dto.MaterialDto
+import com.learn.nasho.databinding.ItemLayoutMaterialBinding
+
+class MaterialAdapter(
+//    private var oldList: List<MaterialDto>,
+    private var onItemClickCallback: RecyclerViewClickListener
+) : RecyclerView.Adapter<MaterialAdapter.MyViewHolder>() {
+
+    private var oldList = emptyList<MaterialDto>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding =
+            ItemLayoutMaterialBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val data = oldList[position]
+
+        Log.d("MaterialAdapter", "RecyclerView Adapter set with ${oldList.size} items.")
+        with(holder.binding) {
+            tvMaterialTitle.text = buildString {
+                append("Materi ")
+                append(position + 1)
+            }
+            tvMaterialDesc.text = data.title
+
+            itemView.setOnClickListener {
+                onItemClickCallback.onItemClicked(position)
+            }
+
+            llLearLock.root.visibility = View.GONE
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return oldList.size
+    }
+
+    fun getItem(index: Int): MaterialDto {
+        return oldList[index]
+    }
+
+    fun setItems(newList: List<MaterialDto>) {
+        oldList = newList
+        notifyDataSetChanged()
+    }
+
+//    fun setItems(newList: List<MaterialDto>) {
+//        Log.d("TAG", "setItems: oldList: $oldList")
+//
+//        // Periksa apakah ada perubahan antara oldList dan newList
+//        if (oldList.isEmpty()) {
+//            oldList = newList
+//        }
+//
+//        val diffUtil = MaterialDiffCallback(oldList, newList)
+//        Log.d("TAG", "setItems: diffUtil ${Gson().toJson(diffUtil)}")
+//        val diffResults = DiffUtil.calculateDiff(diffUtil)
+//        Log.d("TAG", "setItems: diffResults ${Gson().toJson(diffResults)}")
+//        oldList = newList
+//
+//        diffResults.dispatchUpdatesTo(this@MaterialAdapter)  // Jika sudah benar, gunakan ini
+//        // Coba gunakan notifyDataSetChanged() sementara untuk melihat pembaruan
+//        notifyDataSetChanged()  // Digunakan sementara untuk debugging
+//
+//    }
+
+    class MyViewHolder(val binding: ItemLayoutMaterialBinding) :
+        RecyclerView.ViewHolder(binding.root)
+}
